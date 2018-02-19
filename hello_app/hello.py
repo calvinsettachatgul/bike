459e9d365851fee94633f71bc77e0bd42083be15
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2018-02-18 16:36:51 -0800

    add some template

--- hello_app/hello.py
+++ hello_app/hello.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import json 
 
 app = Flask(__name__)
@@ -8,4 +8,5 @@ person1 = { "name": "calvin",
 
 @app.route('/')
 def hello_world():
-    return json.dumps(person1)
+    return render_template('user_show.html', rider=person1)
+    # json.dumps(person1)
