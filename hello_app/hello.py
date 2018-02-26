7027618a2733d8b8f808c1a3f9430229dc63f65c
Wendy M <wendymoltrup@gmail.com>
2018-02-25 16:41:06 -0800

    adding an h1

--- hello_app/hello.py
+++ hello_app/hello.py
@@ -3,10 +3,21 @@ import json
 
 app = Flask(__name__)
 
-person1 = { "name": "calvin", 
-            "location": "San Mateo"}
+#Maybe create a user_input? Maybe pseudocode this 
+#Is there any way to combine all of the riders? 
+
+rider1 = { "name": "calvin", 
+           "location": "San Mateo" } 
+rider2 = {
+	"name": "Wendy",
+	"location": "Palo Alto" }         
+
+rider3 = {
+	"name": "Dana",
+	"location": "Mountain View" }   
 
 @app.route('/')
 def hello_world():
     return render_template('user_show.html', rider=person1)
     # json.dumps(person1)
+
