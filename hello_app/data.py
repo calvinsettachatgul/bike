b8b1846812f2d1611ae3d5ddf23da7f4a48999cb
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2018-03-31 08:16:14 -0700

    added lat lng info around san mateo

--- hello_app/data.py
+++ hello_app/data.py
@@ -3,13 +3,14 @@ users = [
    "last_name": "settachatgul",
    "email": "calvin@example.com",
    "bikeways": [
-                 { "name": "home to work",
-                   "start_loc": { "lat": 123, "lng": 456},
-                   "end_loc": { "lat": 123, "lng": 456 },
+                 { "name": "home to bart",
+                   "start_loc": { "lat": 37.574373, "lng": -122.334168},
+                   "end_loc": { "lat": 37.601092, "lng": -122.386551 },
+
                  },
-                 { "name": "work to home",
+                 { "name": "bart to home",
                    "start_loc": { "lat": 123, "lng": 456},
-                   "end_loc": { "lat": 123, "lng": 456}
+                   "end_loc": { "lat": 37.574373, "lng": -122.334168}
                  }
                 ]
   },
@@ -64,4 +65,37 @@ for user in users:
   print(user["last_name"])
   print(user["bikeways"][0])
 
+'''
+Millbrae
+California 94030
+37.601092, -122.386551
 
+522 N San Mateo Dr
+San Mateo, CA 94401
+37.574373, -122.334168
+
+San Mateo Public Library Downtown
+San Mateo, CA
+37.564988, -122.326465
+
+824 Cowan Rd
+Burlingame, CA 94010
+37.602910, -122.374729
+
+Burlingame Public Library 480 Primrose Rd
+Burlingame, CA 94010
+37.579509, -122.349226
+
+101 San Francisco International Airport Tram
+San Francisco, CA 94128
+37.621593, -122.378966
+
+101 San Francisco International Airport Tram
+San Francisco, CA 94128
+37.621593, -122.378966
+
+101 San Francisco International Airport Tram
+San Francisco, CA 94128
+37.621593, -122.378966
+
+'''
