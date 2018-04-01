244a496035acc9ec9b70934b37b676a024b1a8fb
Wendy M <wendymoltrup@gmail.com>
2018-04-01 16:51:17 -0700

    moved data to data file

---
+++ hello_app/data.py
@@ -0,0 +1,11 @@
+rider1 = { "name": "calvin", 
+           "location": "San Mateo" } 
+rider2 = {
+	"name": "Wendy",
+	"location": "Palo Alto" }         
+
+rider3 = {
+	"name": "Dana",
+	"location": "Mountain View" }   
+
+people = [rider1, rider2, rider3 ]
